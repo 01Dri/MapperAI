80ce78a5753cf113247ea5b98ca9cc3120d4ae98
01Dri <diegomagalhaesdev@gmail.com>
2025-09-03 14:57:01 -0300

    feat: compatibility with web pdf and local file pdf

--- test/MapperAI.Test/PdfMapperTests.cs
+++ test/MapperAI.Test/PdfMapperTests.cs
@@ -5,34 +5,53 @@ using MapperAI.Core.Mappers.Interfaces;
 
 namespace MapperAI.Test;
 
-public class PdfMapperTests : BaseTests
+public class PdfMapperIntegrationTests : BaseTests
 {
     private readonly IPDFMapper _pdfMapper;
 
-    public PdfMapperTests()
+    public PdfMapperIntegrationTests()
     {
-        var clientConfiguration = new MapperClientConfiguration( Environment.GetEnvironmentVariable("GEMINI_KEY"),ModelType.GeminiFlash2_0);
-        _pdfMapper = new PdfMapper(Serializer, Factory, clientConfiguration);
+        var clientConfiguration = new MapperClientConfiguration("AIzaSyCiJxEdi-yzBg5GPsPhh6etWEKXzZATXtU", ModelType.GeminiFlash2_0);
+        _pdfMapper = new PdfMapper(Serializer, Factory, clientConfiguration, new HttpClient());
     }
 
     [Fact]
-    public async Task Test1()
+    public async Task Should_Map_Curriculum_From_GoogleDrive()
     {
-        var pdfPath = Path.Combine(@"../../../Curriculo - Diego.pdf");
-        CurriculumModel? curriculumModel =  await _pdfMapper.MapAsync<CurriculumModel>(pdfPath);
-        Assert.Contains("Uninter", curriculumModel?.Faculdade);
-        Assert.Equal("Análise e desenvolvimento de sistemas EAD", curriculumModel?.Curso);
-        Assert.Equal(2, curriculumModel?.Projects.Count);
-        Assert.Equal("diegomagalhaesdev@gmail.com", curriculumModel?.Email);
-        
-        var expectedProjectNames = new List<string> { "ReclameTrancoso", "VTHoftalon" };
-        var actualProjectNames = curriculumModel?.Projects.Select(p => p.Nome).ToList();
+        var model = await _pdfMapper.MapAsync<CurriculumModel>(
+            "https://drive.google.com/file/d/1ByhxqDtlX2d_jnmxF8kqgPZJNKs54k4R/view?usp=drive_link");
 
-        Assert.Equal(expectedProjectNames, actualProjectNames);
+        Assert.NotNull(model);
+        Assert.Equal("Análise e desenvolvimento de sistemas EAD", model?.Curso);
+        Assert.Contains("Uninter", model?.Faculdade);
+        Assert.Equal("diegomagalhaesdev@gmail.com", model?.Email);
+    }
 
+    [Fact]
+    public async Task Should_Map_PenalCode_From_Senado()
+    {
+        var model = await _pdfMapper.MapAsync<CodigoPenal>(
+            "https://www2.senado.leg.br/bdsf/bitstream/handle/id/529748/codigo_penal_1ed.pdf");
+
+        Assert.NotNull(model);
+        Assert.Equal("Senador Eunício Oliveira", model?.Presidente);
+        Assert.Equal(4, model?.SuplentesDeSecretario.Count);
+        Assert.Contains("Senador Eduardo Amorim", model?.SuplentesDeSecretario);
+    }
+
+    [Fact]
+    public async Task Should_Map_Curriculum_From_LocalFile()
+    {
+        var pdfPath = Path.Combine("../../../Curriculo - Diego.pdf");
+
+        var model = await _pdfMapper.MapAsync<CurriculumModel>(pdfPath);
+
+        Assert.NotNull(model);
+        Assert.Equal("Análise e desenvolvimento de sistemas EAD", model?.Curso);
+        Assert.Equal("diegomagalhaesdev@gmail.com", model?.Email);
+        Assert.True(model?.Projects.Count > 0);
     }
 }
-
 
 public class CurriculumModel
 {
@@ -50,4 +69,11 @@ public class CurriculumProjects
 {
     public string Nome { get; set; }
     public List<string> Tecnologias { get; set; } = new List<string>();
+}
+
+public class CodigoPenal
+{
+    public string? Presidente { get; set;}
+    public List<string> SuplentesDeSecretario { get; set; } = [];
+
 }
